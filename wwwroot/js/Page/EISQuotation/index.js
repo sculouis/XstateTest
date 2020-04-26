41b13273249fdaaab0479b1a56c0932a74b247c9
Louis <scu.louis@gmail.com>
2020-04-26 20:48:42 +0800

    button event ready

--- wwwroot/js/Page/EISQuotation/index.js
+++ wwwroot/js/Page/EISQuotation/index.js
@@ -22,7 +22,6 @@ function lookupItems(looktype) {
             lookType: looktype
         },
         success: function (data) {
-            //console.log(data)
             if (looktype == 'StateProvCd') {
                 var result = _.filter(JSON.parse(data), function (item) {
                     return item.countryCd == 'TW'
@@ -65,234 +64,196 @@ function lookupItems(looktype) {
 //InsInfo => 投保險種資料
 //OtherData => 其他約定事項
 //ReadySubmit => 準備傳送
-
 //設定有限狀態機
 const stateMachine = XState.Machine({
-        id: 'toggle',
-        initial: 'QuoteBasic',
-        states: {
-            //保單基本資料
-            QuoteBasic: {
-                on: {
-                    NextStep: 'SalesManData'
-                },
-                entry:['setQuoteBasicAction'],
-                exit:['setQuoteBasicAction'],
-                activities: ['getcode', 'getaddress', 'getRelToPolicyHolder', 'getBeneficiaryRelToDriver', 'getpointdiff', 'getgrade', 'getHotMonthlyDepreciationRate'],
+    id: 'toggle',
+    initial: 'QuoteBasic',
+    states: {
+        //保單基本資料
+        QuoteBasic: {
+            on: {
+                SalesManData: 'SalesManData'
             },
-            //招攬機構/業務人員資料
-            SalesManData: {
-                on: {
-                    NextStep: 'InsurerData'
-                },
-                entry:['setSalesManDataAction'],
-                exit:['setSalesManDataAction'],
-                activities: [],
-            },
-            //要保人及被保險人資料
-            InsurerData: {
-                on: {
-                    NextStep: 'CarInfo'
-                },
-                entry:['setInsurerDataAction'],
-                exit:['setInsurerDataAction'],
-                activities: [],
-            },
-            //車籍資料
-            CarInfo: {
-                on: {
-                    NextStep: 'InsInfo'
-                },
-                entry:['setCarInfoAction'],
-                exit:['setCarInfoAction'],
-            },
-            //投保險種資料
-            InsInfo: {
-                on: {
-                    NextStep: 'OtherData'
-                },
-                entry:['setInsInfoAction'],
-                exit:['setInsInfoAction'],
-                activities: [],
-            },
-            //其他約定事項
-            OtherData: {
-                on: {
-                    NextStep: 'ReadySumit'
-                },
-                entry:['setOtherDataAction'],
-                exit:['setOtherDataAction'],
-            },
-            //準備新增
-            ReadySumit: {
-            },
+            entry: ['setAction'],
+            exit: ['setUnAction'],
+            activities: ['getcode', 'getaddress', 'getRelToPolicyHolder', 'getBeneficiaryRelToDriver', 'getpointdiff', 'getgrade', 'getHotMonthlyDepreciationRate'],
         },
-        on: {
-            JumpToQuoteBasic: '.QuoteBasic',
-            JumpToSalesManData: '.SalesManData',
-            JumpToInsurerData: '.InsurerData',
-            JumpToCarInfo: '.CarInfo',
-            JumpToInsInfo: '.InsInfo',
-            JumpToOtherData: '.OtherData',
-        }
-    }, {
-        activities: {
-            getcode: function (context, activity) {
-                //console.log(context)
-                //console.log(activity)
-                if (!context.HotVehicleCategory.length) {
-                    //車種	 HotVehicleCategory
-                    BlockUI()
-                    console.time("取得HotVehicleCategory花費時間:")
-                    lookupItems('HotVehicleCategory').done(function (res) {
-                        context.HotVehicleCategory = res
-                        console.timeEnd("取得HotVehicleCategory花費時間:")
-                    })
-                    $.unblockUI()
-                }
+        //招攬機構/業務人員資料
+        SalesManData: {
+            on: {
+                InsurerData: 'InsurerData'
             },
-            getaddress: function (context, activate) {
-                //地址(縣市)	 StateProvCd
-                if (!context.StateProvCd.length) {
-                    console.time("取得StateProvCd花費時間:")
-                    lookupItems('StateProvCd').done(function (res) {
-                        //console.log(result)
-                        context.StateProvCd = res
-                        console.timeEnd("取得StateProvCd花費時間:")
-                    })
-                    console.time("取得CityCd花費時間:")
-                    //地址(鄉鎮市區)	 CityCd
-                    lookupItems('CityCd').done(function (res) {
-                        context.CityCd = res
-                        console.timeEnd("取得CityCd花費時間:")
-                    })
-                }
-            },
-            getRelToPolicyHolder: function (context, activity) {
-                if (!context.RelToPolicyHolder.length) {
-                    //與要保人的關係RelToPolicyHolder
-                    console.time("取得RelToPolicyHolder花費時間:")
-                    lookupItems('RelToPolicyHolder').done(function (res) {
-                        context.RelToPolicyHolder = res
-                        console.timeEnd("取得RelToPolicyHolder花費時間:")
-                    })
-                }
-            },
-            getBeneficiaryRelToDriver: function (context, activity) {
-                //console.log(context)
-                //console.log(activity)
-                if (!context.BeneficiaryRelToDriver.length) {
-                    //與列名駕駛人的關係	 BeneficiaryRelToDriver
-                    console.time("取得BeneficiaryRelToDriver花費時間:")
-                    lookupItems('BeneficiaryRelToDriver').done(function (res) {
-                        context.BeneficiaryRelToDriver = res
-                        console.timeEnd("取得BeneficiaryRelToDriver花費時間:")
-                    })
-                }
-            },
-            getpointdiff: function (context, activity) {
-                //console.log(context)
-                //console.log(activity)
-                if (!context.PointDiff.length) {
-                    //體係	 PointDiff
-                    console.time("取得PointDiff花費時間:")
-                    lookupItems('PointDiff').done(function (res) {
-                        context.PointDiff = res
-                        console.timeEnd("取得PointDiff花費時間:")
-                    })
-                }
-            },
-            getgrade: function (context, activity) {
-                //console.log(context)
-                //console.log(activity)
-                if (!context.Grade.length) {
-                    //責係	 Grade
-                    console.time("取得Grade花費時間:")
-                    lookupItems('Grade').done(function (res) {
-                        context.Grade = res
-                        console.timeEnd("取得Grade花費時間:")
-                    })
-                }
-            },
-            getHotMonthlyDepreciationRate: function (context, activity) {
-                //console.log(context)
-                //console.log(activity)
-                if (!context.HotMonthlyDepreciationRate.length) {
-                    //折舊率	 HotMonthlyDepreciationRate
-                    console.time("取得HotMonthlyDepreciationRate花費時間:")
-                    lookupItems('HotMonthlyDepreciationRate').done(function (res) {
-                        context.HotMonthlyDepreciationRate = res
-                        console.timeEnd("取得HotMonthlyDepreciationRate花費時間:")
-                    })
-                }
-            },
+            entry: ['setAction'],
+            exit: ['setUnAction'],
+            activities: [],
         },
-        actions: {
-            // action implementations
-            setQuoteBasicAction: function(context, event) {
-                if (event.type == "JumpToQuoteBasic" || event.type =="xstate.init"){
-                    context.QuoteBasicAction = 1
-                }else{
-                    if (context.QuoteBasicAction == 1)
-                    {
-                        context.QuoteBasicAction = 2
-                    }else{
-                        context.QuoteBasicAction = 1
+        //要保人及被保險人資料
+        InsurerData: {
+            on: {
+                CarInfo: 'CarInfo'
+            },
+            entry: ['setAction'],
+            exit: ['setUnAction'],
+            activities: [],
+        },
+        //車籍資料
+        CarInfo: {
+            on: {
+                InsInfo: 'InsInfo'
+            },
+            entry: ['setAction'],
+            exit: ['setUnAction'],
+        },
+        //投保險種資料
+        InsInfo: {
+            on: {
+                OtherData: 'OtherData'
+            },
+            entry: ['setAction'],
+            exit: ['setUnAction'],
+            activities: [],
+        },
+        //其他約定事項
+        OtherData: {
+            on: {
+                ReadySumit: 'ReadySumit'
+            },
+            entry: ['setAction'],
+            exit: ['setUnAction'],
+        },
+        //準備新增
+        ReadySumit: {},
+    },
+    on: {
+        JumpToQuoteBasic: '.QuoteBasic',
+        JumpToSalesManData: '.SalesManData',
+        JumpToInsurerData: '.InsurerData',
+        JumpToCarInfo: '.CarInfo',
+        JumpToInsInfo: '.InsInfo',
+        JumpToOtherData: '.OtherData',
+    }
+}, {
+    activities: {
+        getcode: function (context, activity) {
+            if (!context.HotVehicleCategory.length) {
+                //車種	 HotVehicleCategory
+                BlockUI()
+                console.time("取得HotVehicleCategory花費時間:")
+                lookupItems('HotVehicleCategory').done(function (res) {
+                    context.HotVehicleCategory = res
+                    console.timeEnd("取得HotVehicleCategory花費時間:")
+                })
+                $.unblockUI()
+            }
+        },
+        getaddress: function (context, activate) {
+            //地址(縣市)	 StateProvCd
+            if (!context.StateProvCd.length) {
+                console.time("取得StateProvCd花費時間:")
+                lookupItems('StateProvCd').done(function (res) {
+                    //console.log(result)
+                    context.StateProvCd = res
+                    console.timeEnd("取得StateProvCd花費時間:")
+                })
+                console.time("取得CityCd花費時間:")
+                //地址(鄉鎮市區)	 CityCd
+                lookupItems('CityCd').done(function (res) {
+                    context.CityCd = res
+                    console.timeEnd("取得CityCd花費時間:")
+                })
+            }
+        },
+        getRelToPolicyHolder: function (context, activity) {
+            if (!context.RelToPolicyHolder.length) {
+                //與要保人的關係RelToPolicyHolder
+                console.time("取得RelToPolicyHolder花費時間:")
+                lookupItems('RelToPolicyHolder').done(function (res) {
+                    context.RelToPolicyHolder = res
+                    console.timeEnd("取得RelToPolicyHolder花費時間:")
+                })
+            }
+        },
+        getBeneficiaryRelToDriver: function (context, activity) {
+            if (!context.BeneficiaryRelToDriver.length) {
+                //與列名駕駛人的關係	 BeneficiaryRelToDriver
+                console.time("取得BeneficiaryRelToDriver花費時間:")
+                lookupItems('BeneficiaryRelToDriver').done(function (res) {
+                    context.BeneficiaryRelToDriver = res
+                    console.timeEnd("取得BeneficiaryRelToDriver花費時間:")
+                })
+            }
+        },
+        getpointdiff: function (context, activity) {
+            if (!context.PointDiff.length) {
+                //體係	 PointDiff
+                console.time("取得PointDiff花費時間:")
+                lookupItems('PointDiff').done(function (res) {
+                    context.PointDiff = res
+                    console.timeEnd("取得PointDiff花費時間:")
+                })
+            }
+        },
+        getgrade: function (context, activity) {
+            if (!context.Grade.length) {
+                //責係	 Grade
+                console.time("取得Grade花費時間:")
+                lookupItems('Grade').done(function (res) {
+                    context.Grade = res
+                    console.timeEnd("取得Grade花費時間:")
+                })
+            }
+        },
+        getHotMonthlyDepreciationRate: function (context, activity) {
+            if (!context.HotMonthlyDepreciationRate.length) {
+                //折舊率	 HotMonthlyDepreciationRate
+                console.time("取得HotMonthlyDepreciationRate花費時間:")
+                lookupItems('HotMonthlyDepreciationRate').done(function (res) {
+                    context.HotMonthlyDepreciationRate = res
+                    console.timeEnd("取得HotMonthlyDepreciationRate花費時間:")
+                })
+            }
+        },
+    },
+    actions: {
+        //進入此狀態設定為下一步xxxxAction = 1
+        setAction: function (context, event) {
+            console.log(`entry: ${event.type}`)
+            if (event.type.substring(0, 6) == "JumpTo") {
+                var stateName = event.type.substring(6)
+                context.ActionFlag[stateName + 'Action'] = 1
+                //此狀態以下的button全改為隱藏
+                const actionArr = Object.keys(context.ActionFlag);
+                const index = _.findIndex(actionArr, function(item) { return item == stateName + 'Action'; });
+                const disableArr = actionArr.slice(index + 1)
+                _.forEach(disableArr, function(item){
+                    context.ActionFlag[item] = 0
+                })
+            } else {
+                if (event.type != "xstate.init"){
+                context.ActionFlag[event.type + 'Action'] = 1
                 }
             }
-            },
-            setSalesManDataAction: function(context, event) {
-                if (context.SalesManDataAction == 1)
-                {
-                    context.SalesManDataAction = 2
-                }else{
-                    context.SalesManDataAction = 1
-                }
-            },
-            setInsurerDataAction: function(context, event) {
-                if (context.InsurerDataAction == 1)
-                {
-                    context.InsurerDataAction = 2
-                }else{
-                    context.InsurerDataAction = 1
-                }
-            },
-            setCarInfoAction: function(context, event) {
-                if (context.CarInfoAction == 1)
-                {
-                    context.CarInfoAction = 2
-                }else{
-                    context.CarInfoAction = 1
-                }
-            },
-            setInsInfoAction: function(context, event) {
-                if (context.InsInfoAction == 1)
-                {
-                    context.InsInfoAction = 2
-                }else{
-                    context.InsInfoAction = 1
-                }
-            },
-            setOtherDataAction: function(context, event) {
-                if (context.OtherDataAction == 1)
-                {
-                    context.OtherDataAction = 2
-                }else{
-                    context.OtherDataAction = 1
-                }
-            },
-        }
+        },
+        //離開此狀態設定為編輯xxxxAction = 2
+        setUnAction: function (context, event) {
+            console.log(`exit: get into ${event.type}`)
+            const actionArr = Object.keys(context.ActionFlag);
+            const index = _.findIndex(actionArr, function(item) { return item == event.type + 'Action'; });
+            context.ActionFlag[actionArr[index - 1]] = 2
+        },
     }
-);
+});
 
 // define context
 const stateMachineContext = stateMachine.withContext({
-    QuoteBasicAction: 1,
-    SalesManDataAction: 0,
-    InsurerDataAction: 0,
-    CarInfoAction: 0,
-    InsInfoAction: 0,
-    OtherDataAction: 0,
+    ActionFlag: {
+        QuoteBasicAction: 1,
+        SalesManDataAction: 0,
+        InsurerDataAction: 0,
+        CarInfoAction: 0,
+        InsInfoAction: 0,
+        OtherDataAction: 0,
+    },
     HotVehicleCategory: [],
     StateProvCd: [],
     CityCd: [],
@@ -350,6 +311,8 @@ const app = new Vue({
             // Start with the machine's initial context
             context: stateMachineContext.context,
 
+            actionFlag: stateMachineContext.context.ActionFlag,
+
             // Bootstrap column size
             colSize: colSize,
 
@@ -369,7 +332,10 @@ const app = new Vue({
         },
         // 下一步    
         changeAction: function () {
-          this.send('NextStep')
+            var nextState = _.filter(this.quoteService.state.nextEvents, function (e) {
+                return e.substring(0, 6) != 'JumpTo';
+            });
+            this.send(nextState[0])
         },
     }
 })
